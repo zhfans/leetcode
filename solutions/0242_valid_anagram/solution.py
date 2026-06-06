800c452ad3182e1a3579ee1d334b2244d88ca832
Hong Fan Zhao <hongfan.zhao@outlook.com>
2026-06-06 23:04:40 +0200

    2026-06-06

--- solutions/0242_valid_anagram/solution.py
+++ solutions/0242_valid_anagram/solution.py
@@ -1,4 +1,4 @@
-# https://leetcode.com/problems/valid-anagram/description/
+from collections import Counter
 
 
 class Solution:
@@ -6,14 +6,7 @@ class Solution:
         if len(s) != len(t):
             return False
 
-        s_chars_reps: list[int] = [0] * 26
-        for c in s:
-            s_chars_reps[ord(c) - ord("a")] += 1
+        return Counter(s) == Counter(t)
 
-        for c in t:
-            i = ord(c) - ord("a")
-            s_chars_reps[i] -= 1
-            if s_chars_reps[i] < 0:
-                return False
 
-        return True
+# https://leetcode.com/problems/valid-anagram/description/
